4bd40b78f8ff19c3aae86bce23923b51adbfd2df
Clifford Mapesa <cliffordmapesa8@gmail.com>
2022-06-14 12:16:08 +0300

    git commit

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDeauwXGj0Txwzc4ty7z81bvYCsLc4TVmg",
+    authDomain: "whatsapp-clone-98a93.firebaseapp.com",
+    projectId: "whatsapp-clone-98a93",
+    storageBucket: "whatsapp-clone-98a93.appspot.com",
+    messagingSenderId: "990744176490",
+    appId: "1:990744176490:web:5a4d7b59fa4616e9786713",
+    measurementId: "G-BY7NGBX9QF"
+};
+
+firebase.initializeApp(firebaseConfig);
